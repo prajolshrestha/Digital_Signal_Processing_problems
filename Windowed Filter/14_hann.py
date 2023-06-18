f0a2227cf2bcc492e1a7c6009028fc3732e9a205
Prajol Shrestha <prajolstha9@gmail.com>
2023-06-18 13:15:17 +0200

    Add files via upload

---
+++ Windowed Filter/14_hann.py
@@ -0,0 +1,26 @@
+from scipy import signal
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.fftpack import fft, fftshift
+
+#51 point hanning window(Impulse Response)
+window = signal.hann(51)
+
+plt.plot(window)
+plt.title("hanning window")
+
+#Frequency Response
+plt.figure()
+
+A = fft(window,2048)/(len(window)/2.0)
+hz = np.linspace(-0.5,0.5,len(A))
+response = 20*np.log10(np.abs(fftshift(A/abs(A).max())))
+
+plt.plot(hz,response)
+plt.axis([-0.5,0.5,-120,0])
+plt.title('Freq response of hanning window')
+plt.ylabel('Normalized magnitude')
+plt.xlabel('Normalized Frequency')
+
+plt.show()
+
