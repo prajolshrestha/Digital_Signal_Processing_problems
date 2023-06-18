6086104204ba6d34f09d30f5d5426d52554b5d6e
Prajol Shrestha <prajolstha9@gmail.com>
2023-06-18 13:16:31 +0200

    Add files via upload

---
+++ Windowed Filter/18_slepian.py
@@ -0,0 +1,26 @@
+from scipy import signal
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.fftpack import fft, fftshift
+
+#51 point slepian window(Impulse Response)
+window = signal.windows.dpss(51,NW=2*3) #try changing different bandwidth eg. 0.3
+
+plt.plot(window)
+plt.title("slepian window")
+
+#Frequency Response
+plt.figure()
+
+A = fft(window,2048)/(len(window)/2.0)
+hz = np.linspace(-0.5,0.5,len(A))
+response = 20*np.log10(np.abs(fftshift(A/abs(A).max())))
+
+plt.plot(hz,response)
+plt.axis([-0.5,0.5,-120,0])
+plt.title('Freq response of slepian window')
+plt.ylabel('Normalized magnitude')
+plt.xlabel('Normalized Frequency')
+
+plt.show()
+
