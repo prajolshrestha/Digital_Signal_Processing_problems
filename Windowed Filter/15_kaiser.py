40e08c9dd15d46ca4e918ca481bc929a44b89c83
Prajol Shrestha <prajolstha9@gmail.com>
2023-06-18 13:15:31 +0200

    Add files via upload

---
+++ Windowed Filter/15_kaiser.py
@@ -0,0 +1,26 @@
+from scipy import signal
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.fftpack import fft, fftshift
+
+#51 point kaiser window(Impulse Response)
+window = signal.kaiser(51,beta=17) #beta inc garyo vane window narrow hudai janxa
+
+plt.plot(window)
+plt.title("kaiser window")
+
+#Frequency Response
+plt.figure()
+
+A = fft(window,2048)/(len(window)/2.0)
+hz = np.linspace(-0.5,0.5,len(A))
+response = 20*np.log10(np.abs(fftshift(A/abs(A).max())))
+
+plt.plot(hz,response)
+plt.axis([-0.5,0.5,-120,0])
+plt.title('Freq response of kaiser window')
+plt.ylabel('Normalized magnitude')
+plt.xlabel('Normalized Frequency')
+
+plt.show()
+
