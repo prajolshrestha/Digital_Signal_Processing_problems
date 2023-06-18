03b5f29e54c177d036249e9b5bc0a1bc175b1672
Prajol Shrestha <prajolstha9@gmail.com>
2023-06-18 12:45:13 +0200

    Add files via upload

---
+++ Windowed Filter/11_flattop.py
@@ -0,0 +1,26 @@
+from scipy import signal
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.fftpack import fft, fftshift
+
+#51 point flattop window(Impulse Response)
+window = signal.flattop(51)
+
+plt.plot(window)
+plt.title("Flattop window")
+
+#Frequency Response
+plt.figure()
+
+A = fft(window,2048)/(len(window)/2.0)
+hz = np.linspace(-0.5,0.5,len(A))
+response = 20*np.log10(np.abs(fftshift(A/abs(A).max())))
+
+plt.plot(hz,response)
+plt.axis([-0.5,0.5,-120,0])
+plt.title('Freq response of Flattop window')
+plt.ylabel('Normalized magnitude')
+plt.xlabel('Normalized Frequency')
+
+plt.show()
+
