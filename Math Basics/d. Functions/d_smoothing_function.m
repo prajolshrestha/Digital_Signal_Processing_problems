16f405fc5f4bcae917c2d9fa4e18fb5cbd8635f9
Prajol Shrestha <prajolstha9@gmail.com>
2023-07-31 19:15:52 +0200

    Add files via upload

---
+++ Math Basics/d. Functions/d_smoothing_function.m
@@ -0,0 +1,11 @@
+% Smoothing Function with Optional Inputs
+
+% learn : nargin, varargin, error, title
+
+%% 
+
+MM_meansmoothplot(randn(500,1),10,1,'My smoothing function');
+
+MM_meansmoothplot(randn(500,1),10,1,'My new smoothing function');
+
+myzscore('apple')
